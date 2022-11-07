b6eee319ecaef3e933b1dc0cc4b513645c7ff813
jjp9080 <jjp9080@nyu.edu>
2022-11-07 16:03:39 -0500

    Update sketch.js
    Added radios

--- sketch.js
+++ sketch.js
@@ -1,6 +1,4 @@
 let waves1;
-let w = 750;
-let h = 600;
 let sp = 30;
 let n1 = 0.0;
 let m1 = 0.0;
@@ -20,25 +18,22 @@ function setup() {
 
 function draw() {
   background(235, 204, 228);
-  if (frameCount <= 300){
+  if (frameCount <= 200){
    scene_one();
   }
-  if (frameCount > 300 && frameCount <= 800){
+  if (frameCount > 200 && frameCount <= 300){
     scene_two();
   } 
-  if (frameCount > 800 && frameCount <= 1200){
+  if (frameCount > 300 && frameCount <= 400){
     noiseScale = 0.02;
     scene_one();
   }
-  if (frameCount > 1200 && frameCount <= 1500){
+  if (frameCount > 400 && frameCount <= 600){
     scene_two();
-  } 
-  if (frameCount > 1500 && frameCount <= 1520){
-    background(255);
   }
-  if (frameCount > 1520 && frameCount <= 1560){
-    scene_two();
-  } 
+  if (frameCount > 600 && frameCount <= 900){
+    scene_three();
+  }
 }
 function scene_one(){
     waves1.calm2(255, 192, 245);
@@ -56,22 +51,33 @@ function scene_two(){
     j = 100;
     waves1.calm1(12, 4, 222);
 }
+function scene_three(){
+  background(0, 255, 247);
+  for (x = -60; x <= width-250; x += 200) {
+    for (y = -60; y <= height-200; y += 200) {
+      push();
+      translate(x, y);
+      waves1.radio();
+      pop();
+    }
+  }
+}
 class waves{
   constructor (x, y){
     this.x_ = x;
     this.y_ = y;
   }
    calm1(calmcolor1, calmcolor2, calmcolor3){
-    for (let y = 0; y < h; y += 270) {
+    for (let y = 0; y < height; y += 270) {
     strokeWeight(2);
     stroke(calmcolor1, calmcolor2, calmcolor3);
     noFill();
     
     beginShape();
     
-    for (let x = 0; x < w; x+=sp) {
-    	let noiseScale = map(noise(x + m1 * 5, n1 + x * 5), 0, 1, 50+i, 120+j)
-	curveVertex(x, (y+ noiseScale));
+    for (let x = 0; x < width; x+=sp) {
+      let noiseScale = map(noise(x + m1 * 5, n1 + x * 5), 0, 1, 50+i, 120+j)
+  curveVertex(x, (y+ noiseScale));
       m1 += 0.0001;
       n1 += 0.0002;
 
@@ -88,5 +94,19 @@ class waves{
       m += 0.000002;
       n += 0.000001;
     }
+  }
+  radio() {
+    noStroke();
+    fill(56, 2, 19);
+    rect(101, 102, 115, 80, 7);
+    fill(227, 5, 30);
+    rect(110, 102, 115, 80, 7);
+    fill(195, 198, 201);
+    ellipse(140, 140, 40, 40);
+    stroke(0);
+    strokeWeight(3);
+    line(210, 140, 170, 140);
+    line(210, 130, 170, 130);
+    line(210, 150, 170, 150);
   }
 }
